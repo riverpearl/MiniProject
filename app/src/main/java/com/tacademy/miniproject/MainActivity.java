518a6ee2bcea9471be9aa4464d34ba82e4bfac06
River Pearl <riverpearl0115@gmail.com>
2016-08-10 09:58:04 +0900

    friend list

--- app/src/main/java/com/tacademy/miniproject/MainActivity.java
+++ app/src/main/java/com/tacademy/miniproject/MainActivity.java
@@ -2,12 +2,49 @@ package com.tacademy.miniproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.tacademy.miniproject.autodata.User;
+import com.tacademy.miniproject.autodata.UserResult;
+import com.tacademy.miniproject.manager.NetworkManager;
+import com.tacademy.miniproject.manager.NetworkRequest;
+import com.tacademy.miniproject.request.FriendListRequest;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.list_friend)
+    ListView friendsView;
+
+    ArrayAdapter<User> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAdapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1);
+        friendsView.setAdapter(mAdapter);
+
+        FriendListRequest request = new FriendListRequest(this);
+        NetworkManager.getInstance().getNetworkData(request, new NetworkManager.OnResultListener<UserResult<List<User>>>() {
+            @Override
+            public void onSuccess(NetworkRequest<UserResult<List<User>>> request, UserResult<List<User>> result) {
+                List<User> users = result.getResult();
+                mAdapter.addAll(users);
+            }
+
+            @Override
+            public void onFail(NetworkRequest<UserResult<List<User>>> request, int errorCode, String errorMessage, Throwable e) {
+                Toast.makeText(MainActivity.this, "error : " + errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/tacademy/miniproject/autodata/User.java
+++ app/src/main/java/com/tacademy/miniproject/autodata/User.java
@@ -28,4 +28,9 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return userName;
+    }
 }
